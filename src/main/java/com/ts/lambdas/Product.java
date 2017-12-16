4e5db09087888508e63b044473e4df7c3e4919c5
jerrymon <jerilonline@gmail.com>
2017-12-16 12:36:25 -0500

    Lambda mods

--- src/main/java/com/ts/lambdas/Product.java
+++ src/main/java/com/ts/lambdas/Product.java
@@ -2,13 +2,14 @@ package com.ts.lambdas;
 
 public class Product {
 
-	int id;  
-    String name;  
-    float price;  
-    public Product(int id, String name, float price) {  
-        super();  
-        this.id = id;  
-        this.name = name;  
-        this.price = price;  
-    }  
+	int id;
+	String name;
+	float price;
+
+	public Product(int id, String name, float price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
 }
